51c20f7833ce20609ce94f7205921da9e0d33e56
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-09-25 17:02:31 +0700

    feature: adding DB Context

---
+++ WebApi/Data/BookingManagementDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Data
+{
+    public class BookingManagementDbContext : DbContext
+    {
+        public BookingManagementDbContext(DbContextOptions<BookingManagementDbContext> options) : base(options) {  }
+
+        // Add Models to Migrate
+        public DbSet<University> Universities { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Accounts> Accounts { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<AccountRole> AccountRoles { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //menambahkan constraint unique
+            modelBuilder.Entity<Employee>().HasIndex(e => new
+            {
+                e.Nik,
+                e.Email,
+                e.PhoneNumber
+            }).IsUnique();
+        }
+    }
+}
