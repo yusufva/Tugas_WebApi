816b8987f0ba6cadc7fb960e11ff0de331ce2298
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-09-25 13:52:13 +0700

    fix: changing nullable LastName

--- WebApi/Models/Employee.cs
+++ WebApi/Models/Employee.cs
@@ -5,11 +5,13 @@
         public Guid Guid { get; set; }
         public string Nik {  get; set; }
         public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? LastName { get; set; }
         public DateTime BirthDate { get; set; }
         public int Gender { get; set; }
         public DateTime HiringDate { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+
+
     }
 }
