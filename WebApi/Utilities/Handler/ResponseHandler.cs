f5782672a8948214db0a54a963502fdf086423f0
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-03 17:09:52 +0700

    feature: adding response handler

---
+++ WebApi/Utilities/Handler/ResponseHandler.cs
@@ -0,0 +1,85 @@
+﻿using System.Net;
+
+namespace WebApi.Utilities.Handler
+{
+    //Handler Response OK
+    public class ResponseOkHandler<Tentity>
+    {
+        public int Code { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public Tentity? Data { get; set; }
+
+        public ResponseOkHandler(Tentity? data, string message)
+        {
+            Code = StatusCodes.Status200OK;
+            Status = HttpStatusCode.Created.ToString();
+            Message = message;
+            Data = data;
+        }
+
+        public ResponseOkHandler(string message)
+        {
+            Code = StatusCodes.Status200OK;
+            Status = HttpStatusCode.Created.ToString();
+            Message = message;
+        }
+    }
+
+    //Handler Response Bad Request
+    public class ResponseBadRequestHandler
+    {
+        public int Code { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public string? Error { get; set; }
+
+        public ResponseBadRequestHandler(string message, string error)
+        {
+            Code = StatusCodes.Status400BadRequest;
+            Status = HttpStatusCode.BadRequest.ToString();
+            Message = message;
+            Error = error;
+        }
+    }
+
+    //Handler Response Not Found
+    public class ResponseNotFoundHandler
+    {
+        public int Code { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public string? Error { get; set; }
+
+        public ResponseNotFoundHandler(string message)
+        {
+            Code = StatusCodes.Status404NotFound;
+            Status = HttpStatusCode.NotFound.ToString();
+            Message = message;
+        }
+        public ResponseNotFoundHandler(string message, string error)
+        {
+            Code = StatusCodes.Status404NotFound;
+            Status = HttpStatusCode.NotFound.ToString();
+            Message = message;
+            Error = error;
+        }
+    }
+
+    //Handler Response Internal Server Error
+    public class ResponseInternalServerErrorHandler
+    {
+        public int Code { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public string? Error { get; set; }
+
+        public ResponseInternalServerErrorHandler(string message, string error)
+        {
+            Code = StatusCodes.Status500InternalServerError;
+            Status = HttpStatusCode.InternalServerError.ToString();
+            Message = message;
+            Error = error;
+        }
+    }
+}
