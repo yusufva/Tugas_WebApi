7964dfa6be19d32a71a7f263a1895e9b517777af
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-03 12:05:02 +0700

    feature: adding hashHandler class

---
+++ WebApi/Utilities/Handler/HashHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApi.Utilities.Handler
+{
+    public class HashHandler
+    {
+        private static string GetRandomSalt()
+        {
+            return BCrypt.Net.BCrypt.GenerateSalt(13);
+        }
+
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, GetRandomSalt());
+        }
+
+        public static bool ValidatePassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
