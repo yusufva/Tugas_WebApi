9305c6199416df2d92f245483a15b166d07ebb89
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-05 14:49:59 +0700

    feature: adding change password dto

---
+++ WebApi/DTOs/Account/ChangePasswordRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.DTOs.Account
+{
+    public class ChangePasswordRequestDto
+    {
+        public string Email { get; set; }
+        public int Otp {  get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
