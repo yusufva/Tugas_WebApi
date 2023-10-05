6ad3db85c220332bc8fc6520ac6f943fe0d2b603
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-05 17:18:01 +0700

    feature: Implement email handler

---
+++ WebApi/Utilities/Handler/EmailHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Mail;
+using WebApi.Contracts;
+
+namespace WebApi.Utilities.Handler
+{
+    public class EmailHandler : IEmailHandler
+    {
+        private string _server;
+        private int _port;
+        private string _fromEmailAddress;
+
+        public EmailHandler(string server, int port, string fromEmailAddress)
+        {
+            _server = server;
+            _port = port;
+            _fromEmailAddress = fromEmailAddress;
+        }
+
+        public void Send(string subject, string body, string toEmail)
+        {
+            var message = new MailMessage()
+            {
+                From = new MailAddress(_fromEmailAddress),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+
+            message.To.Add(new MailAddress(toEmail)); //add tujuan email
+
+            using var smtpClient = new SmtpClient(_server, _port); //instance smtp client
+            smtpClient.Send(message); //ngirim
+
+        }
+    }
+}
