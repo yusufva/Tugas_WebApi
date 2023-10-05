4b9dbf97f9605e01b807338d9eb95a084cfdd8d3
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-05 14:47:50 +0700

    feature: adding generate otp handler

---
+++ WebApi/Utilities/Handler/OtpHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi.Utilities.Handler
+{
+    public class OtpHandler
+    {
+        public static string GenerateRandomOtp()
+        {
+            var random = new Random();
+            int otpValue = random.Next(100000, 999999);
+            return otpValue.ToString();
+        }
+    }
+}
