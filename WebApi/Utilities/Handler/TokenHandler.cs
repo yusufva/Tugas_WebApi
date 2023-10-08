ae36dc1272a831cf4f8b52970390ed5da7aade9b
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-08 13:37:05 +0700

    feature: adding tokenhandler utilities

---
+++ WebApi/Utilities/Handler/TokenHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebApi.Contracts;
+
+namespace WebApi.Utilities.Handler
+{
+    public class TokenHandler : ITokenHandler
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenHandler(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(IEnumerable<Claim> claims)
+        {
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWTService:SecretKey"]));
+            var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            var tokenOption = new JwtSecurityToken(issuer: _configuration["JWTService:Issuer"], 
+                audience:_configuration["JWTService:Audience"], 
+                claims:claims, 
+                expires: DateTime.Now.AddHours(5), 
+                signingCredentials: signingCredentials);
+            var encodedToken = new JwtSecurityTokenHandler().WriteToken(tokenOption);
+
+            return encodedToken;
+        }
+    }
+}
