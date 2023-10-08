6cdfccf836f2f0eab72d70d7135f7e9e4e43c321
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-08 13:37:34 +0700

    feature: implementing jwt and cors

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,14 +1,19 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 using WebApi.Contracts;
 using WebApi.Data;
+using Microsoft.IdentityModel.Tokens;
 using WebApi.DTOs.Employees;
 using WebApi.Repositories;
 using WebApi.Utilities.Handler;
 using WebApi.Utilities.Validations.Employees;
+using System.Text;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,12 +32,13 @@ builder.Services.AddScoped<IAccountsRepository, AccountsRepository>(); //mengins
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>(); //menginstance Employee Repo
 builder.Services.AddScoped<GenerateHandler>(); //menginstance Generate Handler
 
-// Add email Service to Controller
+// Add Handler Service to Controller
 builder.Services.AddTransient<IEmailHandler, EmailHandler>(_ => new EmailHandler(
         builder.Configuration["SmtpService:Server"],
         int.Parse(builder.Configuration["SmtpService:Port"]),
         builder.Configuration["SmtpService:FromEmailAddress"]
     ));
+builder.Services.AddScoped<ITokenHandler, WebApi.Utilities.Handler.TokenHandler>();
 
 builder.Services.AddControllers()
     .ConfigureApiBehaviorOptions(options =>
@@ -51,9 +57,69 @@ builder.Services.AddControllers()
 builder.Services.AddFluentValidationAutoValidation()
     .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
+//JWT Authentication Configuration
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false; //dev only
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["JWTService:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["JWTService:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTService:SecretKey"])),
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
+//add cors
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin();
+policy.AllowAnyHeader();
+policy.WithMethods("GET", "POST", "DELETE", "PUT", "OPTIONS");
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x => {
+    x.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Metrodata Coding Camp",
+        Description = "ASP.NET Core API 6.0"
+    });
+    x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+    x.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -65,6 +131,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
