88bf4551cf2624b12a987c884a39d03134ffe562
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-10-08 13:36:20 +0700

    feature: adding more booking DTO

---
+++ WebApi/DTOs/Bookings/BookedTodayDto.cs
@@ -0,0 +1,23 @@
+﻿using WebApi.Models;
+using WebApi.Utilities.Enums;
+
+namespace WebApi.DTOs.Bookings
+{
+    public class BookedTodayDto
+    {
+        public Guid BookingGuid { get; set; }
+        public string RoomName { get; set; }
+        public StatusLevel Status {  get; set; }
+        public int Floor {  get; set; }
+        public string BookedBy {  get; set; }
+
+        public static explicit operator BookedTodayDto(Booking booking)
+        {
+            return new BookedTodayDto
+            {
+                BookingGuid = booking.Guid,
+                Status = booking.Status,
+            };
+        }   
+    }
+}
---
+++ WebApi/DTOs/Bookings/BookingDetailDto.cs
@@ -0,0 +1,17 @@
+﻿using WebApi.Utilities.Enums;
+
+namespace WebApi.DTOs.Bookings
+{
+    public class BookingDetailDto
+    {
+        public Guid Guid { get; set; }
+        public string BookedNik { get; set; }
+        public string BookedBy { get; set; }
+        public string RoomName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public StatusLevel Status {  get; set; }
+        public string Remarks { get; set; }
+
+    }
+}
